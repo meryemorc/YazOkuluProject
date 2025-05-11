dd8103de1a74659bc5bec4331d27a931bcbf6e9d
meryem <116671202+meryemorc@users.noreply.github.com>
2025-05-11 17:14:52 +0300

    📱 Kotlin mobil uygulama klasörü eklendi

---
+++ yazokulumobile/app/src/main/java/com/oruc/yazokulumobile/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.oruc.yazokulumobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.oruc.yazokulumobile.ui.theme.YazOkuluMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            YazOkuluMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    YazOkuluMobileTheme {
+        Greeting("Android")
+    }
+}
